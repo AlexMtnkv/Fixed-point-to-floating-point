73ea6203f319a62427818d9379c0982f86f4aed5
AlexMtnkv <90116073+AlexMtnkv@users.noreply.github.com>
2022-08-17 23:19:49 +0400

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -8,7 +8,7 @@ int main()
     print_FromScale(118112256, 28, 28, 1, 1.0);
     print_FromScale(255, 7, 8, 1, 0.0);
     print_FromScale(16711680, 23, 8, 0, 0.0);
-    print_FromScale(16711680, 23, 4, 1, 3.0);
+    print_FromScale(16711680, 23, 8, 1, 0.0);
     print_FromScale(983040, 19, 4, 0, 3.0);
     print_FromScale(983040, 19, 4, 1, 3.0);
     print_FromScale(65536, 16, 1, 0, 0.1);
